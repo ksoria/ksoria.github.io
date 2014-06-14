2a8650dd316d8657ca16f86b18df406be4832c65
ksoria <ksoria15@smpanthers.org>
2014-06-14 08:41:44 -0700

    Update madworldpage.js

--- madworldpage.js
+++ madworldpage.js
@@ -96,7 +96,7 @@ $(document).keydown(function(e) {
     $.getScript('http://www.cornify.com/js/cornify.js',function(){
       cornify_add();
       $(document).keydown(cornify_add);
-    document.getElementById('higurashi').src='picture_1.jpg'"
+    document.getElementById('higurashi').src='10 Never Gonna Give You Up.wma '
     });
   }
 });
